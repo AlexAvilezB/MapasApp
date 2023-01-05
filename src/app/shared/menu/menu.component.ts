e80a2d78dc55b1aa7f0cd13bf801c8a64460b7c7
Alejandro Avilez <aleantonio656@gmail.com>
2023-01-04 19:32:27 -0600

    Menu de navegacion

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  ruta: string;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
-  ]
+    `
+    li {
+      cursor: pointer;
+    }
+    `
+  ],
 })
 export class MenuComponent {
-
+  menuItems = [
+    {
+      ruta: '/mapas/fullscreen',
+      nombre: 'Full Screen',
+    },
+    {
+      ruta: '/mapas/zoom-range',
+      nombre: 'Zoom Range',
+    },
+    {
+      ruta: '/mapas/propiedades',
+      nombre: 'Propiedades',
+    },
+    {
+      ruta: '/mapas/marcadores',
+      nombre: 'Marcadores',
+    },
+  ];
 }
